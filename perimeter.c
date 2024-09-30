5e842ec0925f0fbae537d7d31c2b89257ca4fc48
Syamala-devi-123 <syamaladevilakshmipuram@gmail.com>
2024-09-30 12:31:09 +0530

    Create perimeter.c

---
+++ perimeter.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int l,b,perimeter;
+    printf("enter the values of l,b");
+    scanf("%d%d",&l,&b);
+    perimeter = 2*(l+b);
+    printf("The perimeter of a rectangle  %d",perimeter);
+}
