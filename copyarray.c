2a8bc2f4d22fdb019708a21bc9936a24f94e4b22
Syamala-devi-123 <syamaladevilakshmipuram@gmail.com>
2025-01-10 12:29:54 +0530

    Create copyarray.c

---
+++ copyarray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,n,b[10];
+    printf("enter size");
+    scanf("%d",&n);
+    printf("enter the elements");
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;++i)
+    {
+        b[i]=a[i];
+    }
+    for(i=0;i<n;++i)
+    {
+        printf(" %d",b[i]);
+    }
+}
