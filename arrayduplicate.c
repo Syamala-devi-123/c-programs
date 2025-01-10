3fd4a23711e96c48d35334f2d85de62176cb1fa9
Syamala-devi-123 <syamaladevilakshmipuram@gmail.com>
2025-01-10 12:17:49 +0530

    Create arrayduplicate.c

---
+++ arrayduplicate.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,j,n,count=0;
+    printf("enter size");
+    scanf("%d",&n);
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;++i)
+    {
+        for(j=i+1;j<n;++j)
+        {
+            if(a[i]==a[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    printf("the duplicate elements in the array %d",count);
+}
